42334647d6c6c3e850534bd603a913479dfbd04f
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2023-04-27 12:48:37 -0300

    fix: css do login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -56,7 +56,7 @@ class Login extends Component {
                 required
               />
             </label>
-            <label>
+            <label className="login-label">
               Senha:
               <input
                 type="password"
